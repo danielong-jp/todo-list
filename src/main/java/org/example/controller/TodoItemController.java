6ac52d932d3dac8054f646c5d246afb3b98f3e2f
Daniel Ong <crimsonknightd@hotmail.com>
2020-07-14 14:12:14 +0900

    Added services for views for adding items, home, item list.

---
+++ src/main/java/org/example/controller/TodoItemController.java
@@ -0,0 +1,58 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.model.TodoData;
+import org.example.model.TodoItem;
+import org.example.service.TodoItemService;
+import org.example.util.AttributeNames;
+import org.example.util.Mappings;
+import org.example.util.ViewNames;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Controller
+public class TodoItemController {
+
+    // == fields ==
+    private final TodoItemService todoItemService;
+
+    // == constructors ==
+    @Autowired
+    public TodoItemController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
+    // == model attributes ==
+    @ModelAttribute
+    public TodoData todoData() {
+        return todoItemService.getData();
+    }
+
+    // == handler methods ==
+    // http://localhost:8080/todo-list/items
+    @GetMapping(Mappings.ITEMS)
+    public String items() {
+        return ViewNames.ITEMS_LIST;
+    }
+
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model) {
+        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
+        log.info("todoItem from form = {}", todoItem);
+        todoItemService.addItem(todoItem);
+        return "redirect:/" + Mappings.ITEMS;
+    }
+}
