040bcadaf2fcb2b08b44e541c7e32b021687f339
Daniel Ong <crimsonknightd@hotmail.com>
2020-07-14 14:20:03 +0900

    Implemented deleteItem

--- src/main/java/org/example/controller/TodoItemController.java
+++ src/main/java/org/example/controller/TodoItemController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 
@@ -42,11 +43,20 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    // http://localhost:8080/todo-list/add_item
     @GetMapping(Mappings.ADD_ITEM)
     public String addEditItem(Model model) {
         TodoItem todoItem = new TodoItem("", "", LocalDate.now());
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
+    }
+
+    // http://localhost:8080/todo-list/deleteItem?id=
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        log.info("Deleting item with id= {}", id);
+        todoItemService.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
     }
 
     @PostMapping(Mappings.ADD_ITEM)
